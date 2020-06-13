f1e64ad860e79bd77911057503d5621c66cd8eee
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-06-13 12:16:00 +0530

    new design

---
+++ water/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/nav';
+import Menu from './components/menu';
+
+function App() {
+  return (
+    <div className="App">
+        <Nav/>
+        <Menu />
+
+    </div>
+  );
+}
+
+export default App;
